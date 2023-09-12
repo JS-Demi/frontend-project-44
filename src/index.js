916fcf2ad5fb493d6fe5fa2f4062f7db8c3a832c
Dmitry <dmt.ivanov1@yandex.ru>
2023-09-12 20:51:39 +0300

    fix src/cli.js & index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from "readline-sync";
 
 const brainGames = (game) => {
   console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May i have your name? ");
+  const userName = readlineSync.question("May I have your name? ");
   console.log("Hello, " + userName + "!");
   let result = 0;
   while (result < 3) {

55f8245f43cdfa8d2a982f411289c88950671acc
Dmitry <dmt.ivanov1@yandex.ru>
2023-09-12 15:04:46 +0300

    add src/games

---
+++ src/games/brain-calc.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import brainGames from '../index.js';
+
+const brainCalc = () => {
+    console.log('What is the result of the expression?');
+    const operands = ['+', '-', '*'];
+    const index = Math.floor(Math.random() * 3);
+    const randomOperand = operands[index];
+    const calc = (n1, n2) => {
+    if (randomOperand === '+') return n1 + n2;
+    if (randomOperand === '-') return n1 - n2;
+    return n1 * n2;
+    }
+    const randomNumber1 = Math.round(Math.random() * 20);
+    const randomNumber2 = Math.round(Math.random() * 10);
+    console.log(`Question: ${randomNumber1} ${randomOperand} ${randomNumber2}`);
+    const expression = calc(randomNumber1, randomNumber2);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    if (+yourAnswer === +expression) {
+        console.log('Correct!');
+        return 'Correct!';
+    }
+    const lose = `'${yourAnswer}' is wrong answer ;(. Correct answer was '${expression}'.\nLet's try again, `
+    return lose;
+};
+export default () => brainGames(brainCalc)

38015e3668c855ea26ed91e3d443b2f72458b454
Dmitry <dmt.ivanov1@yandex.ru>
2023-09-12 22:02:36 +0300

    fix src/games/primeNumber.js & gcdGame.js

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -4,20 +4,19 @@ import readlineSync from "readline-sync";
 const brainGcd = () => {
   console.log("Find the greatest common divisor of given numbers.");
   const calc = (n1, n2) => {
-    if (n1 > n2) {
-      for (let i = n2; i >= 1; i--) {
-        if (n1 % i === 0 && n2 % i === 0) {
-          return +i;
-        }
-      }
+    let i;
+    if (n1 > n2) { 
+      i = n2;
     } else {
-      for (let i = n1; i >= 1; i--) {
-        if (n1 % i === 0 && n2 % i === 0) {
-          return +i;
-        }
-      }
+      i = n1;
     }
-  };
+    while (i >= 1) {
+      if (n1 % i === 0 && n2 % i === 0) {
+        return +i;
+      }
+      i -= 1;
+    }
+    };
   const randomNumber1 = Math.ceil(Math.random() * 20);
   const randomNumber2 = Math.ceil(Math.random() * 20);
   console.log(`Question: ${randomNumber1} ${randomNumber2}`);
@@ -27,7 +26,7 @@ const brainGcd = () => {
     console.log("Correct!");
     return "Correct!";
   }
-  const lose = `'${yourAnswer}' is wrong answer ;(. Correct answer was '${expression}'.\nLet's try again, `;
-  return lose;
+  return `'${yourAnswer}' is wrong answer ;(. Correct answer was '${expression}'.\nLet's try again, `;
+  
 };
 export default () => brainGames(brainGcd);

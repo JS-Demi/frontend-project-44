81b5f2186cfbef8b144baacc6730effc0eff2d55
Dmitry <dmt.ivanov1@yandex.ru>
2023-09-13 11:15:40 +0300

    fix src/games/progressionGame.js

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -1,32 +1,26 @@
-import readlineSync from 'readline-sync';
 import brainGames from '../index.js';
 
-const gameCondition = 'What number is missing in the progression?';
+const rules = 'What number is missing in the progression?';
+
+const newProgression = () => {
+  const rndNumb1 = Math.round(Math.random() * 50);
+  const rndNumb2 = Math.ceil(Math.random() * 10);
+  const arr = [rndNumb1];
+  let progression = rndNumb1;
+  for (let i = 1; i < 10; i += 1) {
+    progression += rndNumb2;
+    arr.push(progression);
+  }
+  return arr;
+};
 
 const brainProgression = () => {
-  const newProgression = () => {
-    const rndNumb = Math.round(Math.random() * 50);
-    const rndNumb2 = Math.ceil(Math.random() * 10);
-    const arr = [rndNumb];
-    let progression = rndNumb;
-    for (let i = 1; i < 10; i += 1) {
-      progression += rndNumb2;
-      arr.push(progression);
-    }
-    return arr;
-  };
-  const rndNumb3 = Math.round(Math.random() * 9);
+  const rndNumb = Math.round(Math.random() * 9);
   const progressionArr = newProgression();
-  const skippedNumber = progressionArr
+  const skippedNumberArr = progressionArr
     .join(' ')
-    .replace(progressionArr[rndNumb3], '..');
-  console.log(`Question: ${skippedNumber}`);
-  const yourAnswer = readlineSync.question('Your answer: ');
-  if (+yourAnswer === +progressionArr[rndNumb3]) {
-    console.log('Correct!');
-    return 'Correct!';
-  }
-  const lose = `'${yourAnswer}' is wrong answer ;(. Correct answer was '${progressionArr[rndNumb3]}'.\nLet's try again, `;
-  return lose;
+    .replace(progressionArr[rndNumb], '..');
+  const data = [skippedNumberArr, progressionArr[rndNumb]];
+  return data;
 };
-export default () => brainGames(gameCondition, brainProgression);
+export default () => brainGames(brainProgression, rules);

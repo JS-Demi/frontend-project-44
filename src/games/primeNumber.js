45568889406a773174f2dc7a096496a18b63b601
Dmitry <dmt.ivanov1@yandex.ru>
2023-09-13 11:15:24 +0300

    fix src/games/primeNumber.js

--- src/games/primeNumber.js
+++ src/games/primeNumber.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
 import brainGames from '../index.js';
 
-const gameCondition = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (n) => {
   for (let i = 2; i < n; i += 1) {
@@ -14,17 +13,9 @@ const isPrime = (n) => {
 
 const primeNumber = () => {
   const randomNumber = Math.ceil(Math.random() * 100);
-  console.log(`Question: ${randomNumber}`);
-  const yourAnswer = readlineSync.question('Your answer: ');
   if (isPrime(randomNumber)) {
-    if (yourAnswer === 'yes') {
-      console.log('Correct!');
-      return 'Correct!';
-    } return `"${yourAnswer}" is wrong answer ;(. Correct answer was "yes".\nLet's try again, `;
+    return [randomNumber, 'yes'];
   }
-  if (yourAnswer === 'no') {
-    console.log('Correct!');
-    return 'Correct!';
-  } return `"${yourAnswer}" is wrong answer ;(. Correct answer was "no".\nLet's try again, `;
+  return [randomNumber, 'no'];
 };
-export default () => brainGames(gameCondition, primeNumber);
+export default () => brainGames(primeNumber, rules);
